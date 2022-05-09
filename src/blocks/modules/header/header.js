40aa0f197529e25be9c9dbb6d72b4aebbe3203d9
ilyaSizov <sizov.ilya1996@gmail.com>
2022-05-09 14:11:55 +0300

    second time update

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -3,8 +3,11 @@
 const menuBurger = document.querySelector(".icon-menu");
 const menuEls = document.querySelectorAll(".menu__list-link");
 let menuEl = document.querySelector(".menu__content");
+let headerMenuEl = document.querySelector(".header__menu");
 
 menuBurger.addEventListener("click", () => {
   menuBurger.classList.toggle("_active");
   menuEl.classList.toggle("_active");
+  menuEl.classList.toggle("_active");
+  headerMenuEl.classList.toggle("_active");
 });
